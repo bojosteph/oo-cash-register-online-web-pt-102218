2029574af40def69e8e1ba81d79382d608555304
Roberto Samia <bojosteph@gmail.com>
2018-11-15 00:30:16 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,6 +5,18 @@ class CashRegister
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+    @cart []
+  end
+  
+  def add_item(item, price, quantity = 1)
+    item_hash = {}
+    
+    item_hash[:item] = item
+    item_hash[:price] = price
+    item_hash[:quantity] = quantity
+    
+    @cart << item_hash
+    @total += price * quantity
   end
   
   
