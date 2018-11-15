91dbabb2ac0f34de4cbf935a94ed1e50536be1e5
Roberto Samia <bojosteph@gmail.com>
2018-11-15 01:23:47 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,10 +8,10 @@ class CashRegister
     @cart = []
   end
   
-  def add_item(title, amount, quantity=1)
+  def add_item(name, price, quan
     self.total += amount * quantity
     quantity.times do
-      items << title
+      cart << 
     end
     self.last_transaction = amount * quantity
   end
@@ -25,16 +25,13 @@ class CashRegister
     end
   end
   
-  def items
-    item_names = []
-    @cart.each do | item_info |
-      #test expects product name * quantity...
-      for qty in 1..item_info[:quantity] 
-        item_names << item_info[:item]
-      end 
-    end 
-    item_names
-  end 
+  def add_item(title, amount, quantity=1)
+    self.total += amount * quantity
+    quantity.times do
+      carttitle
+    end
+    self.last_transaction = amount * quantity
+  end
       
   def void_last_transaction
     
